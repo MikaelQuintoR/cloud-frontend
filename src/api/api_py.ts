77e1800841cdcb1e90151402074f9f6e5ecde352
MikaelQuintoR <mikael.quinto@utec.edu.pe>
2025-05-11 14:46:49 -0500

    avz

---
+++ src/api/api_py.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+
+const axiosInstancePy = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+
+export default axiosInstancePy;
